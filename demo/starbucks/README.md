e00f7e8636950cbf6a42bc3e94ffa3ddd8585465
Florian Kogon <F.Kogon@gmail.com>
2021-08-11 10:36:59 +0200

    first-commit

---
+++ demo/starbucks/README.md
@@ -0,0 +1,1 @@
+docker run -e PORT=8000 -p 8080:8000 starbucks:latest
---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(
+    name='test_create_new_package',
+    version='0.0.1',
+    packages=['create_starbucks_pkg'],
+    scripts=['scripts/starbucks_pkg']
+)
